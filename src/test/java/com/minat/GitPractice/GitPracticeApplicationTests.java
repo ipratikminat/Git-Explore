1bb629887a92363737233653691de7c868b9a924
Pratik Patel <ipratikminat@gmail.com>
2026-03-04 21:46:54 -0600

    Initial Commit

---
+++ src/test/java/com/minat/GitPractice/GitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minat.GitPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
