07d926f8052003d68c1e2eb32f955893e2bc97ec
Pratik Patel <ipratikminat@gmail.com>
2026-03-04 22:12:30 -0600

    PP-1234: added and printed feature 4

--- src/main/java/com/minat/GitPractice/GitPracticeApplication.java
+++ src/main/java/com/minat/GitPractice/GitPracticeApplication.java
@@ -10,6 +10,7 @@ public class GitPracticeApplication {
         System.out.println("Feature 1");
         System.out.println("Feature 2");
         System.out.println("Feature 3");
+        System.out.println("Feature 4");
 		SpringApplication.run(GitPracticeApplication.class, args);
 	}
 
