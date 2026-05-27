52559acd0d2b30dc5c00ccb36fc13d4258387c90
Pratik Patel <ipratikminat@gmail.com>
2026-05-26 23:00:52 -0500

    PP-123: Removed 2,3,4, & 5 from Github portal itself

--- src/main/java/com/minat/GitPractice/GitPracticeApplication.java
+++ src/main/java/com/minat/GitPractice/GitPracticeApplication.java
@@ -8,10 +8,6 @@ public class GitPracticeApplication {
 
 	public static void main(String[] args) {
         System.out.println("Feature 1");
-        System.out.println("Feature 2");
-        System.out.println("Feature 3");
-        System.out.println("Feature 4");
-        System.out.println("Feature 5");
 		SpringApplication.run(GitPracticeApplication.class, args);
 	}
 
